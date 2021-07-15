a42966aaad3be8c7c697f3d764495e5620c1a64a
Jhonny millones <jhonny.millones@gmail.com>
2021-07-15 17:53:16 -0500

    refactor(Login): Cleane code do not used

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,22 +1,20 @@
-import LoginBanner from './login-banner/LoginBanner'
+import LoginBanner from './login-banner/LoginBanner';
 
-import { Grid } from '@material-ui/core'
+import { Grid } from '@material-ui/core';
 import '@fontsource/lato';
 import LoginData from './login-data/LoginData';
 
 const Login = () => {
   return (
     <div className="Login" style={{ height: '100vh' }}>
-
       <Grid
         container
         alignItems="center"
         justifyContent="center"
-        align="center" justify="center"
+        align="center"
         style={{ minHeight: '100vh' }}
       >
         <Grid item xs={12} sm={5}>
-          {/* <Paper className={classes.paper}>xs=3</Paper> */}
           <LoginBanner></LoginBanner>
         </Grid>
         <Grid item xs={12} sm={7}>
@@ -24,10 +22,7 @@ const Login = () => {
         </Grid>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
-/* 
-<LoginBanner></LoginBanner>
-*/
-export default Login
+export default Login;
--- src/pages/login/login-data/LoginData.js
+++ src/pages/login/login-data/LoginData.js
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
 import { TextField, Button, Grid } from '@material-ui/core';
 import Controls from '../../../components/controls/Controls';
+import { useHistory } from 'react-router-dom';
 
 // import FormGroup from '@material-ui/core/FormGroup';
 import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
-const LoginData = () => {
-
-  const [phone, setPhone] = useState('')
-  const [errorPhone, setErrorPhone] = useState('')
+const LoginData = (_, context) => {
+  const history = useHistory();
+  const [phone, setPhone] = useState('');
+  const [errorPhone, setErrorPhone] = useState('');
   const validatePhone = (val) => {
-    setPhone(val)
-    val?.length === 9 ? setErrorPhone('') : setErrorPhone('Ingrese un número de 9 dígitos')
-  }
+    setPhone(val);
+    val?.length === 9
+      ? setErrorPhone('')
+      : setErrorPhone('Ingrese un número de 9 dígitos');
+  };
+
+  const handleQuote = () => {
+    history.push('plan');
+  };
 
   return (
-
-    <form noValidate autoComplete="off" className="LoginData" >
+    <form noValidate autoComplete="off" className="LoginData">
       <Grid container spacing={2}>
         <Grid item xs={12}>
           Déjanos tus datos {phone}
@@ -30,7 +36,7 @@ const LoginData = () => {
             label="Celular"
             variant="outlined"
             p={2}
-            onChange={e => validatePhone(e.target.value)}
+            onChange={(e) => validatePhone(e.target.value)}
           />
         </Grid>
 
@@ -41,7 +47,6 @@ const LoginData = () => {
             label="Placa"
             variant="outlined"
           />
-
         </Grid>
         <Grid item xs={12} m={2}>
           <FormControlLabel
@@ -49,26 +54,22 @@ const LoginData = () => {
             label="Custom color"
           />
           <div>
-            Acepto la Política de Protecciòn de Datos Personales y los Términos y Condiciones.
+            Acepto la Política de Protecciòn de Datos Personales y los Términos
+            y Condiciones.
           </div>
         </Grid>
         <Grid item xs={6}>
-          <Button variant="contained" color="primary">COTÍZALO</Button>
-
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => handleQuote()}
+          >
+            COTÍZALO
+          </Button>
         </Grid>
-
       </Grid>
-
-
     </form>
-  )
-}
+  );
+};
 
-export default LoginData
-
-/*
-
-className={classes.root}
-
-control={<Checkbox checked={state.checkedG} onChange={handleChange} name="checkedG" />}
-*/
+export default LoginData;
